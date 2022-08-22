8193016f01048cf4e03d61c38c23722f365b385f
Javier Ayala <franciscojavier20002017@gmail.com>
2022-08-21 19:58:12 -0500

    Agrege el menu principal a la vista principal y ademas agrege los html que se van a usar

--- src/main/java/com/SistemaContable/Controller/MenuPrincipal.java
+++ src/main/java/com/SistemaContable/Controller/MenuPrincipal.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class MenuPrincipal {
@@ -12,4 +13,36 @@ public class MenuPrincipal {
         model.addAttribute("dato","Hola Mundo");
         return "index";
     }
+    @GetMapping("/Catalogo")
+    public String catalogo(Model model){
+        model.addAttribute("dato","Hola Mundo");
+        return "catalogo";
+    }
+    @GetMapping("/LibroDiario")
+    public String LibroDiario(Model model){
+        model.addAttribute("dato","Hola Mundo");
+        return "LibroDiario";
+    }
+    @GetMapping("/LibroMayor")
+    public String LibroMayor(Model model){
+        model.addAttribute("dato","Hola Mundo");
+        return "LibroMayor";
+    }
+    @GetMapping("/BalanceDeComprobacion")
+    public String BalanceDeComprobacion(Model model){
+        model.addAttribute("dato","Hola Mundo");
+        return "BalanceDeComprobacion";
+    }
+    @GetMapping("/EstadoDeResultado")
+    public String EstadoDeResultado(Model model){
+        model.addAttribute("dato","Hola Mundo");
+        return "EstadoDeResultado";
+    }
+
+    @GetMapping("/BalanceGeneral")
+    public String BalanceGeneral(Model model){
+        model.addAttribute("dato","Hola Mundo");
+        return "BalanceGeneral";
+    }
+
 }
