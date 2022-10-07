3ee0c3a9c093dccb6e6915d62705029442e34a3a
Diego Mejia <mejiadiego17@17gmail.com>
2022-10-07 00:35:19 -0600

    añadiendo debe y haber para generar una comparativa

--- src/main/java/com/SistemaContable/Controller/DiegoMejia/RegistroPartidaController.java
+++ src/main/java/com/SistemaContable/Controller/DiegoMejia/RegistroPartidaController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
-import java.util.Optional;
+import java.math.BigDecimal;
+
 
 @Controller
 public class RegistroPartidaController {
@@ -17,25 +18,40 @@ public class RegistroPartidaController {
     String registroPartida(Model model, HttpSession session) {
 
 
-
-        if (session.getAttribute("partidaSesion") == null){
+        if (session.getAttribute("partidaSesion") == null) {
             return "redirect:/LibroDiario";
         }
-        Partida  p = (Partida)session.getAttribute("partidaSesion");
+
+        Partida p = (Partida) session.getAttribute("partidaSesion");
+
+        //suma de los valores debe y haber para luego mostrar en la vista
+        BigDecimal sDebe = new BigDecimal("0");
+        BigDecimal sHaber = new BigDecimal("0");
+        for (RegistroPartida registroPartida: p.getRegistroPartidas()) {
+           sDebe = sDebe.add(registroPartida.getDebe());
+           sHaber =  sHaber.add(registroPartida.getHaber());
+        }
+
+        //instancia para ser utilizada en el formulario de la vista
         RegistroPartida registroPartida = new RegistroPartida();
+
+
         model.addAttribute("registro", registroPartida);
-        model.addAttribute("p",p);
+        model.addAttribute("p", p);
+        model.addAttribute("haber", sHaber);
+        model.addAttribute("debe", sDebe);
         return "RegistroPartida";
 
 
     }
-    @PostMapping("/LibroDiario/registroPartida/add")
-    String addRegistroPartida(HttpSession session, RegistroPartida registroPartida){
 
-        if (session.getAttribute("partidaSesion") == null){
+    @PostMapping("/LibroDiario/registroPartida/add")
+    String addRegistroPartida(HttpSession session, RegistroPartida registroPartida) {
+
+        if (session.getAttribute("partidaSesion") == null) {
             return "redirect:/LibroDiario";
         }
-        Partida  p = (Partida)session.getAttribute("partidaSesion");
+        Partida p = (Partida) session.getAttribute("partidaSesion");
         p.getRegistroPartidas().add(registroPartida);
         return "redirect:/LibroDiario/registroPartida";
 
