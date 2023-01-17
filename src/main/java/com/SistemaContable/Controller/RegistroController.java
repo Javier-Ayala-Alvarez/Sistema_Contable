49624231c4287930a937bf72e6582baaa30636b1
Alejandro <105890159+Deras071@users.noreply.github.com>
2023-01-17 12:27:49 -0600

    Cuentas del mayor

--- src/main/java/com/SistemaContable/Controller/RegistroController.java
+++ src/main/java/com/SistemaContable/Controller/RegistroController.java
@@ -5,9 +5,13 @@ package com.SistemaContable.Controller;
 
 import com.SistemaContable.servicio.JavierAyala.Interfaces.UsuarioServicio;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 
@@ -16,6 +20,9 @@ public class RegistroController {
     //@Autowired es una dependencia para inyectar una clase en otra
     @Autowired
     private UsuarioServicio servicio;
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
 
 
 
@@ -51,6 +58,26 @@ public class RegistroController {
     @GetMapping("/LibroMayor")
     public String LibroMayor(Model model) {
         model.addAttribute("tituloDeLaPagina", "Libro Mayor");
+
+        //CONSULTA LIBRO MAYOR
+        String sql = "SELECT c.codigo, c.nombre, concat(EXTRACT(DAY from p.fecha),'/',EXTRACT(MONTH from p.fecha),'/',EXTRACT(YEAR from p.fecha))AS fecha, p.descripcion, rp.debe, rp.haber, CASE WHEN (c.saldo_cuenta) = 'Deudor' THEN (SUM(rp.debe) - SUM(rp.haber)) ELSE (SUM(rp.haber) - SUM(rp.debe)) END AS saldo FROM Partida p INNER JOIN registro_partida rp ON p.id = rp.partida_id INNER JOIN catalogo c ON c.id = rp.catalogo_id GROUP BY c.codigo, c.nombre, p.fecha, p.descripcion, rp.debe,rp.haber, c.saldo_cuenta ORDER BY c.codigo";
+        List<Map<String, Object>> mayor = jdbcTemplate.queryForList(sql);
+        model.addAttribute("mayor", mayor);
+
+        //ENCABEZADO MAYOR
+        String sql2 = "SELECT c.codigo, c.nombre\n" +
+                "FROM Partida p \n" +
+                "INNER JOIN registro_partida rp ON p.id = rp.partida_id \n" +
+                "INNER JOIN catalogo c ON c.id = rp.catalogo_id \n" +
+                "GROUP BY c.codigo, c.nombre\n" +
+                "ORDER BY c.codigo";
+        List<Map<String, Object>> eMayor = jdbcTemplate.queryForList(sql2);
+        model.addAttribute("eMayor", eMayor);
+
+        String sql3 = "SELECT UPPER(nombre_empresa) as nombre_empresa FROM empresa WHERE id = ?";
+        String nombreEmpresa = jdbcTemplate.queryForObject(sql3, new Object[]{1}, String.class);
+        model.addAttribute("nombreEmpresa", nombreEmpresa);
+
         return "LibroMayor";
     }
     @GetMapping("/Cierre")
