7068795cb2504f8ba45fc191f99680f5fce7c426
g3r50nGit <gersonstanleyescobarserrano@gmail.com>
2023-01-18 15:39:37 -0600

    Actualizando la BDC

--- src/main/java/com/SistemaContable/Controller/Gerson/BalanzaDeComprobacionController.java
+++ src/main/java/com/SistemaContable/Controller/Gerson/BalanzaDeComprobacionController.java
@@ -1,5 +1,7 @@
 package com.SistemaContable.Controller.Gerson;
 
+import com.SistemaContable.Repository.JavierAyala.Interfaces.CicloContableReposytory;
+import com.SistemaContable.model.JavierAyala.CicloContable;
 import com.SistemaContable.servicio.DiegoMejia.Interfaces.PartidaServiceApi;
 import com.SistemaContable.servicio.Gerson.BalanzaDeComprobacionInterfaz;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,21 +11,29 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 @Controller
 public class BalanzaDeComprobacionController {
 
     @Autowired
     PartidaServiceApi partidaServiceApi;
+    @Autowired
+    CicloContableReposytory cicloContableReposytory;
 
     @RequestMapping("/BalanceDeComprobacion")
     public String balanceGeneral(Model model){
-        model.addAttribute("cuentas",partidaServiceApi.mayorizar());
+        Optional<CicloContable> objCiclo =cicloContableReposytory.findFirstByOrderByIdDesc();
+        if (!objCiclo.isPresent()) {
+            return "redirect:/";
+        }
+
+        model.addAttribute("cuentas",partidaServiceApi.mayorizar(objCiclo.get().getId()));
         BigDecimal debeacum = new BigDecimal("0");
         BigDecimal haberacum = new BigDecimal("0");
-        for (int i=0; i<partidaServiceApi.mayorizar().size()-1; i++){
-            debeacum = debeacum.add(partidaServiceApi.mayorizar().get(i).getDebe());
-            haberacum = haberacum.add(partidaServiceApi.mayorizar().get(i).getHaber());
+        for (int i=0; i<partidaServiceApi.mayorizar(objCiclo.get().getId()).size()-1; i++){
+            debeacum = debeacum.add(partidaServiceApi.mayorizar(objCiclo.get().getId()).get(i).getDebe());
+            haberacum = haberacum.add(partidaServiceApi.mayorizar(objCiclo.get().getId()).get(i).getHaber());
         }
         model.addAttribute("debeacum", debeacum);
         model.addAttribute("haberacum", haberacum);
